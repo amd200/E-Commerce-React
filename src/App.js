3c6c8f3671af2c1bf4aede5b5aed4042bdc23533
Ahmed Mohamed <ahm12d.m@gmail.com>
2024-03-19 23:32:59 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/Home/Home";
+import NavBarLogin from "./Components/Uitily/NavBarLogin";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./Components/Uitily/Footer";
+import LoginPage from "./Pages/Auth/LoginPage";
+import RegisterPage from "./Pages/Auth/RegisterPage";
+import AllCategoryPage from "./Pages/Category/AllCategoryPage";
+import AllBrandPage from "./Pages/Brand/AllBrandPage";
+import ShopProductsPage from "./Pages/Products/ShopProductsPage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBarLogin />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/allcategory" element={<AllCategoryPage />} />
+          <Route path="/allbrand" element={<AllBrandPage />} />
+          <Route path="/products" element={<ShopProductsPage />} />
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
